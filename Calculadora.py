cf303d04dfbd745a563a595fc6f0ba8cafdbdbc5
Luiz <luiz04faria@gmail.com>
2024-09-18 21:26:59 -0300

    Added: colors and frames, changed: root and functions, deleted: buttons.

--- Calculadora.py
+++ Calculadora.py
@@ -1,76 +1,63 @@
 from tkinter import *
+import tkinter as ttk
 import tkinter as tk
 import math
 
 
+#colors
+color1 = "#202020"
+color2 = "#ffffff"
+color3 = "#323232"
+color4 = "#3c3c3c"
+color5 = "#4cc2ff"
+color6 = "#47b1e8"
+color7 = "#000000"
+
 
 #functios
-def buttonInput(button_name):
-    current_text = display_var.get()
-    new_text = current_text + button_name
-    display_var.set(new_text)
+showValue = StringVar()
+values = ''
+
+def inputValue(event):
+    global value
+    values = values + str(event)
+    showValue.set(values)
+
+def calculate():
+    global values
+    result = eval(values)
+    showValue.set(str(result))
+    values = str(result)
 
 def clean():
-    display_var.set("")
-
-def calculator():
-    try:
-        expression = display_var.get()
-        expression = expression.replace(',', '.')
-        expression = expression.replace('√', 'math.sqrt')
-        result = eval(expression, {"__builtins__": None}, {"sqrt": math.sqrt, "math": math})
-        display_var.set(str(result))
-    except:
-        display_var.set("Error")
+    global values
+    values = ''
+    showValue.set('')
 
 
-#window 
-root = Tk()
-root.title("Calculator")
-root.configure(background="#202020")
-root.minsize(320,500)
-root.geometry("320x500+500+100") 
+#window
+window = Tk()
+window.title("Calculadora")
+window.geometry("320x500")
+window.config(bg=color1)
+
+#frames
+frameDisplay = Frame(window, width=320, height=130, bg=color2)
+frameDisplay.grid(row=0, column= 0)
+
+frameKeyboard = Frame(window, width=320, height=0, bg=color1)
+frameKeyboard.grid(row=1, column=0)
+
+frameFooter = Frame(window, width=320, height=0, bg=color1)
+frameFooter.grid(row=2, column= 0)
+
 
 #icon
 icon = PhotoImage(file="C:/Users/Luiz Henrique/Desktop/Projetos/Python/Projetos/Calculadora/calculator.png")
-root.iconphoto(False, icon)
-
-#diplay variable
-display_var = tk.StringVar()
-
-#display
-display_label = tk.Label(root, textvariable=display_var, font=("Arial", 20), bg="#202020", fg="white", anchor ="e")
-display_label.grid(row=0, column=0,columnspan=4,sticky="nsew", padx=10, pady=10) 
-
-#buttons 
-
-for i in range(5):
-    root.grid_rowconfigure(i, weight=1)
-for i in range(4):
-    root.grid_columnconfigure(i, weight=1)
+window.iconphoto(False, icon)
 
 
-buttons = [
-    ('7', 1, 0),  ('8', 1, 1),  ('9', 1, 2), ('/', 1, 3), ('C', 1, 4),
-     ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3), (':D', 2 , 4),
-     ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3), ('%', 3, 4),
-    (',',4, 0), ('0', 4, 1), ('+', 4, 2), ('=', 4, 3), ('√', 4, 4)   
-]
-
-for (text, row, coll) in buttons:
-    if text == '=' :
-        button = tk.Button(root, text=text, padx=20, pady=20, command=calculator)
-    elif text == 'C':
-        button = tk.Button(root, text=text, padx=20, pady=20, command=clean)
-    elif text == '√':
-        button = tk.Button(root, text=text, padx=20, pady=20, command= lambda: buttonInput('√'))
-    else:
-        button = tk.Button(root, text=text, padx=20, pady=20, command= lambda t=text: buttonInput(t))
-    button.grid(row=row, column=coll, sticky="nsew")
-
-
-
-root.mainloop()
+window.mainloop()
 
 
 
