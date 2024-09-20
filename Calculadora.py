9078a0f7bda81a2fc99b52b807babe58078951e7
Luiz <luiz04faria@gmail.com>
2024-09-19 21:56:32 -0300

    Added: Buttons, colors and etc.

--- Calculadora.py
+++ Calculadora.py
@@ -5,13 +5,25 @@ import math
 
 
 #colors
-color1 = "#202020"
-color2 = "#ffffff"
-color3 = "#323232"
-color4 = "#3c3c3c"
-color5 = "#4cc2ff"
-color6 = "#47b1e8"
-color7 = "#000000"
+color1 = "#202020" #black
+color2 = "#ffffff" #white
+color3 = "#323232" #dark grey
+color4 = "#3c3c3c" #dark grey2
+color5 = "#4cc2ff" #blue
+color6 = "#47b1e8" #blue2
+color7 = "#000000" #black2
+color8 = "#ffc900" #yellow 
+color9 = "#ffdc00" #yellow2
+color10= "#f10000" #red 
+color11= "#f91c1c" #red2
+color12= "#44d81f" #green 
+color13= "#4ceb25" #green2
+
+#window
+window = Tk()
+window.title("Calculadora")
+window.geometry("323x395")
+window.config(bg=color1)
 
 
 #functios
@@ -19,7 +31,7 @@ showValue = StringVar()
 values = ''
 
 def inputValue(event):
-    global value
+    global values
     values = values + str(event)
     showValue.set(values)
 
@@ -35,26 +47,83 @@ def clean():
     showValue.set('')
 
 
-#window
-window = Tk()
-window.title("Calculadora")
-window.geometry("320x500")
-window.config(bg=color1)
-
 #frames
-frameDisplay = Frame(window, width=320, height=130, bg=color2)
+frameDisplay = Frame(window, width=323, height=120, bg=color2)
 frameDisplay.grid(row=0, column= 0)
 
-frameKeyboard = Frame(window, width=320, height=0, bg=color1)
+frameKeyboard = Frame(window, width=323, height=250, bg=color1)
 frameKeyboard.grid(row=1, column=0)
 
-frameFooter = Frame(window, width=320, height=0, bg=color1)
+frameFooter = Frame(window, width=323, height=15, bg=color1)
 frameFooter.grid(row=2, column= 0)
 
 
 #icon
 icon = PhotoImage(file="C:/Users/Luiz Henrique/Desktop/Projetos/Python/Projetos/Calculadora/calculator.png")
 window.iconphoto(False, icon)
+
+
+#label 
+labelScreen = Label(frameDisplay, textvariable=showValue, bg=color1, fg= color2, font=("ivy 24 bold"), width=16, padx=8, pady=40, justify="right", anchor='e', relief='flat')
+labelScreen.pack()
+
+labelFooter= Label(frameFooter, text="V.: 1.3.2", bg=color1, fg=color2, font=("ivy 9 bold"), width=44, padx=8, justify="right", anchor="e")
+labelFooter.pack()
+
+#buttons 
+btn1 = Button(frameKeyboard, command= clean, text="C", width=17, height=2, bg=color10, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color11, activeforeground=color7, highlightthickness=0, borderwidth=0)
+btn1.place(x=0, y=0)
+
+btn2 = Button(frameKeyboard, command= lambda: inputValue('%'), text='%', width=8, height=2, bg=color8, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color9, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn2.place(x=162, y=0)
+
+btn3 = Button(frameKeyboard, command= lambda: inputValue('/'), text='/', width=8, height=2, bg=color8, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color9, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn3.place(x=241.5, y=0)
+
+btn4 = Button(frameKeyboard, command= lambda: inputValue('7'), text='7', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn4.place(x=0, y=49)
+
+btn4 = Button(frameKeyboard, command= lambda: inputValue('8'), text='8', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn4.place(x=80, y=49)
+
+btn5 = Button(frameKeyboard, command= lambda: inputValue('9'), text='9', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn5.place(x=161, y=49)
+
+btn6 = Button(frameKeyboard, command= lambda: inputValue('*'), text='X', width=8, height=2, bg=color8, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color9, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn6.place(x=241, y=49)
+
+btn7 = Button(frameKeyboard, command= lambda: inputValue('4'), text='4', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn7.place(x=0, y=98)
+
+btn8 = Button(frameKeyboard, command= lambda: inputValue('5'), text='5', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn8.place(x=80, y=98)
+
+btn9 = Button(frameKeyboard, command= lambda: inputValue('6'), text='6', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn9.place(x=161, y=98)
+
+btn10 = Button(frameKeyboard, command= lambda: inputValue('-'), text='-', width=8, height=2, bg=color8, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color9, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn10.place(x=241, y=98)
+
+btn11 = Button(frameKeyboard, command= lambda: inputValue('1'), text='1', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn11.place(x=0, y=147)
+
+btn12 = Button(frameKeyboard, command= lambda: inputValue('2'), text='2', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn12.place(x=80, y=147)
+
+btn13 = Button(frameKeyboard, command= lambda: inputValue('3'), text='3', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn13.place(x=161, y=147)
+
+btn14 = Button(frameKeyboard, command= lambda: inputValue('+'), text='+', width=8, height=2, bg=color8, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color9, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn14.place(x=241, y=147)
+
+btn15 = Button(frameKeyboard, command= lambda: inputValue('0'), text='0', width=17, height=2, bg=color12, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color13, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn15.place(x=0, y=195)
+
+btn16 = Button(frameKeyboard, command= lambda: inputValue('.'), text='.', width=8, height=2, bg=color3, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color4, activeforeground=color2, highlightthickness=0, borderwidth=0 )
+btn16.place(x=161, y=195)
+
+btn17 = Button(frameKeyboard, command= calculate, text='=', width=8, height=2, bg=color5, fg=color2, font=("ivy 11 bold"), relief="flat", overrelief="raised", activebackground=color6, activeforeground=color7, highlightthickness=0, borderwidth=0 )
+btn17.place(x=241, y=195)
 
 
 window.mainloop()
